1c58801c8707b72d3b026bf61d597af1c87954d2
grapysoda <chihhsing_chen@innodisk.com>
2025-04-12 21:19:41 +0800

    chore: remove unuse endpoints and rename the func

--- main.cpp
+++ main.cpp
@@ -6,30 +6,14 @@
 
 using json = nlohmann::json;
 
-void root_handler(const httplib::Request&, httplib::Response& res) {
-    res.set_content("Hello, world! This is C++ backend.", "text/plain");
-}
-
-void api_hello_handler(const httplib::Request&, httplib::Response& res) {
-    json response = {
-        {"message", "Hello from API"},
-        {"status", "success"},
-        {"version", 1.0}
-    };
-
-    res.set_content(response.dump(), "application/json");
-}
-
-void api_tags_handler(const httplib::Request&, httplib::Response& res) {
+void apiTagsHandler(const httplib::Request&, httplib::Response& res) {
     json response = json::parse(std::ifstream("dummy_response/api_tags.json"));
 
     res.set_content(response.dump(), "application/json");
 }
 
 void setup_routes(httplib::Server& svr) {
-    svr.Get("/", root_handler);
-    svr.Get("/api/hello", api_hello_handler);
-    svr.Get("/api/tags", api_tags_handler);
+    svr.Get("/api/tags", apiTagsHandler);
 }
 
 int main() {
