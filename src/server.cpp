d845dcc2f97e6a7ca934f5ecad3e2a33153fd109
grapysoda <chihhsing_chen@innodisk.com>
2025-04-12 23:09:31 +0800

    style: change func name

--- src/server.cpp
+++ src/server.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <httplib.h>
 
-void setup_routes(httplib::Server& svr)
+void setupEndpoints(httplib::Server& svr)
 {
     svr.Get(API::Endpoints::TAGS, API::Handler::tags);
     svr.Get(API::Endpoints::PS, API::Handler::ps);
@@ -15,7 +15,7 @@ int main()
 {
     httplib::Server svr;
 
-    setup_routes(svr);
+    setupEndpoints(svr);
 
     std::cout << "Server is running at http://localhost:11434\n";
     svr.listen("0.0.0.0", 11434);
